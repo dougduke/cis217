26b53a668de694c28c3d7ddb948894e041ab7d56
dougduke <doug.duke@hotmail.com>
2014-08-18 11:12:04 -0700

    818 Rebuild

--- buy.php
+++ buy.php
@@ -5,13 +5,17 @@
    * Date: 7/31/14
    * Time: 11:50 AM
    */
+  $get = ""
+     for( item in $_GET[] ) {
+       $get += $_GET[item] + " "
+     }
 
   $form = <<<EOF
 
 <form method=post action="purchase.php?" + buyUrl>
   Name: <input type="text" name="name" /><br />
   Address: <input type="address" name="address"><br />
-  <input type="hidden" name="purchase" value="' . print_r( $_GET ) . '" />
+  <input type="hidden" name="purchase" value=$get />
   <input type="submit" name="submit" value="Submit Order" />
 </form>
 
