df08833ad152b1d24bf91eb187228bec7679e55b
dougduke <doug.duke@hotmail.com>
2014-08-18 11:25:10 -0700

    818 Rebuild

--- buy.php
+++ buy.php
@@ -5,23 +5,27 @@
    * Date: 7/31/14
    * Time: 11:50 AM
    */
-  $get = ""
-     for( item in $_GET[] ) {
-       $get += $_GET[item] + " "
-     }
+  $get = "";
+  foreach( $_GET[ pid ] as $value ) {
+    $get += $value + " ";
+  };
+  foreach( $_GET[ qty ] as $value ) {
+    $get += $value + " ";
+  }
+
 
   $form = <<<EOF
 
 <form method=post action="purchase.php?" + buyUrl>
   Name: <input type="text" name="name" /><br />
   Address: <input type="address" name="address"><br />
-  <input type="hidden" name="purchase" value=$get />
+//  <input type="hidden" name="purchase" value=$get />
   <input type="submit" name="submit" value="Submit Order" />
 </form>
 
 EOF;
 
   echo json_encode( $form );
-console.log( print_r($_GET));
-//test
+  console . log( print_r( $_GET ) );
+  //test
 
